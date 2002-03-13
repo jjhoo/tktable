5386a3f1ea487b6ad802e3c5dbe1f8bbbf0e7f3e
hobbs <hobbs>
2002-03-13 20:42:21 +0000

    * demos/loadtable.tcl: 	* tests/tkTable.test: adapted load to new configure style where 	unix gets the 'lib' suffix for the library.

--- demos/loadtable.tcl
+++ demos/loadtable.tcl
@@ -2,10 +2,15 @@
 #
 # Ensures that the table library extension is loaded
 
-set table(library) Tktable[info sharedlibextension]
+set ::VERSION 2.8
+if {[string compare unix $tcl_platform(platform)]} {
+    set table(library) Tktable$::VERSION[info sharedlibextension]
+} else {
+    set table(library) libTktable$::VERSION[info sharedlibextension]
+}
 if {
     [string match {} [info commands table]]
-    && [catch {package require Tktable} err]
+    && [catch {package require Tktable $::VERSION} err]
     && [catch {load [file join [pwd] $table(library)]} err]
     && [catch {load [file join [pwd] .. unix $table(library)]} err]
     && [catch {load [file join [pwd] .. win $table(library)]} err]
